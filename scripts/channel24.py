9996fada3dcbf389777a6502103827ccd6ee456d
Robiul Hossain <78794958+robiulhossain1@users.noreply.github.com>
2024-03-28 21:15:56 +0600

    Update channel24.py

--- scripts/channel24.py
+++ scripts/channel24.py
@@ -35,7 +35,7 @@ def grab(url):
     print(hd[st:].strip())
 
 print("#EXTM3U")
-print("#EXT-X-STREAM-INF")
+print("#EXT-X-STREAM-INF:BANDWIDTH=3")
 s = requests.Session()
 with open("../information/channel24.txt") as f:
     for line in f:
